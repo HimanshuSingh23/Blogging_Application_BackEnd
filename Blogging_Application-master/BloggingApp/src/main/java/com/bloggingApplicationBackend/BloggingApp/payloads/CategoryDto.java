e9468df1c120a5bf98110ed8acdd765fc3914800
Himanshu Singh <himanshusingh792502gmail.com>
2024-08-14 08:40:14 +0530

    added validations in categoryapi

--- Blogging_Application-master/BloggingApp/src/main/java/com/bloggingApplicationBackend/BloggingApp/payloads/CategoryDto.java
+++ Blogging_Application-master/BloggingApp/src/main/java/com/bloggingApplicationBackend/BloggingApp/payloads/CategoryDto.java
@@ -1,6 +1,8 @@
 package com.bloggingApplicationBackend.BloggingApp.payloads;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +13,12 @@ import lombok.Setter;
 public class CategoryDto {
 
     private Integer categoryId;
+
+    @NotBlank
+    @Size(min = 4,message = "Minimum size of 4 characters is required")
     private String categoryTitle;
+
+    @NotBlank
+    @Size(min = 4,message = "Minimum size of 4 characters is required")
     private String categoryDescription;
 }
