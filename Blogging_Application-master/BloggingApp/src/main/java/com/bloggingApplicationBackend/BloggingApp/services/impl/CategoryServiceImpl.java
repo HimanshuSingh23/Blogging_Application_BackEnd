b23d5c9434b3bba23fd650fa6cb741b6e0d92845
Himanshu Singh <himanshusingh792502gmail.com>
2024-08-14 07:12:20 +0530

    CategoryApi

--- Blogging_Application-master/BloggingApp/src/main/java/com/bloggingApplicationBackend/BloggingApp/services/impl/CategoryServiceImpl.java
+++ Blogging_Application-master/BloggingApp/src/main/java/com/bloggingApplicationBackend/BloggingApp/services/impl/CategoryServiceImpl.java
@@ -7,10 +7,14 @@ import com.bloggingApplicationBackend.BloggingApp.repositories.CategoryRepo;
 import com.bloggingApplicationBackend.BloggingApp.services.CategoryService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+
+@Service
 public class CategoryServiceImpl implements CategoryService {
 
     @Autowired
